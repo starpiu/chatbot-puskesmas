86455a7575c01f6c6a24c894cc81f24390be6ffb
unknown <fiyazulkhaq.fz.218@gmail.com>
2026-01-29 11:58:55 +0700

    new

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const sessionClient = new dialogflow.SessionsClient({
    CHAT API
 ====================== */
 
-app.post("/api/chat", async (req, res) => {
+app.post("/chat", async (req, res) => {
   const { message } = req.body;
 
   if (!message) {
